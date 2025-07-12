5058bc0b00810b8b5b8d50c3407ff04d5754364c
LostLuma <lilly@lostluma.net>
2025-07-12 11:08:27 +0200

    Improve container image
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

---
+++ src/bin/healthcheck.rs
@@ -0,0 +1,22 @@
+use std::{
+    env,
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+fn main() {
+    let port = env::var("HOST")
+        .unwrap_or("80".to_string())
+        .parse::<u16>()
+        .expect("valid port (u16)");
+
+    let mut stream = TcpStream::connect(("127.0.0.1", port)).expect("tcp connect");
+
+    let data = "GET /health HTTP/1.1\r\n\r\n";
+    stream.write_all(data.as_bytes()).expect("write data");
+
+    let mut buffer = [0; 12];
+    stream.read(&mut buffer).expect("read data");
+
+    assert!(buffer == "HTTP/1.1 200".as_bytes(), "response is not ok");
+}
