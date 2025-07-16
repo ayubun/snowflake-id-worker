81a0a851a6c1e958b023b63537364eec69354fd4
Ethan Breder <ethan.breder@discordapp.com>
2025-07-16 04:49:12 +0000

    gracefully shutdown on control+c/SIGTERM

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use snowflake_id_worker::run_worker;
+use tokio::signal;
 
 #[tokio::main]
 async fn main() {
-    run_worker().await;
+    let handle = tokio::spawn(async {
+        run_worker().await;
+    });
+
+    match signal::ctrl_c().await {
+        Ok(()) => {},
+        Err(err) => {
+            eprintln!("Unable to listen for shutdown signal: {}", err);
+            // we also shut down in case of error
+        },
+    }
+    handle.abort();
 }
