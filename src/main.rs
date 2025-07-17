330ad4cbcc395ea0463d265c049ac47682912b94
Ethan Breder <exbreder@gmail.com>
2025-07-17 15:50:17 -0700

    gracefully shutdown on control+c/SIGINT (#3)
    * gracefully shutdown on control+c/SIGTERM
    
    * enable SIGTERM
    
    * code review
    
    ---------
    
    Co-authored-by: ayu <ayu@ayu.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use snowflake_id_worker::run_worker;
+use snowflake_id_worker::{exit_signal, run_worker};
 
 #[tokio::main]
 async fn main() {
-    run_worker().await;
+    tokio::select!(
+        _ = exit_signal() => println!("Exiting from signal"),
+        _ = run_worker() => println!("Worker exited"),
+    )
 }
